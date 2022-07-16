8ba0ddc5770146c00d5f685090f6cf312f9388e5
Jose Antonio Ponqueli Conto <zehconto@gmail.com>
2022-07-16 18:51:34 -0300

    fix: fixing header test

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,7 +8,8 @@ import { useCart } from '../../hooks/useCart';
 
 const Header = (): JSX.Element => {
   const { cart } = useCart();
-  const cartSize = cart.reduce((sumTotal, product) => sumTotal + product.amount, 0);
+  //const cartSize = cart.reduce((sumTotal, product) => sumTotal + product.amount, 0);
+  const cartSize = cart.length;
 
   return (
     <Container>
